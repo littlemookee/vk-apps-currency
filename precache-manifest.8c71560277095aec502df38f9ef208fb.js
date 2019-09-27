0a2a4f22397852388c13004da6df62a9b868b61d
mikhail <zulkarneev@mail.ru>
2019-09-27 14:56:26 +0300

    Updates

---
+++ precache-manifest.8c71560277095aec502df38f9ef208fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ff642002dd8bf44ed13f1fa7b62edf",
+    "url": "/vk-apps-currency/index.html"
+  },
+  {
+    "revision": "f6857b33b726b8dbcdd9",
+    "url": "/vk-apps-currency/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "23967006fe3093a1c7e2",
+    "url": "/vk-apps-currency/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f6857b33b726b8dbcdd9",
+    "url": "/vk-apps-currency/static/js/2.acec94d4.chunk.js"
+  },
+  {
+    "revision": "23967006fe3093a1c7e2",
+    "url": "/vk-apps-currency/static/js/main.00ead144.chunk.js"
+  },
+  {
+    "revision": "b8d3459a5b273f04b24e",
+    "url": "/vk-apps-currency/static/js/runtime-main.470be02b.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-apps-currency/static/media/persik.4e1ec840.png"
+  }
+]);
